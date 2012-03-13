d69c4399da67878fde6b583d1da866d2528994ef
ralph <ralph@rvdh.de>
2012-03-13 15:29:32 +0100

    add name validation for items

--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'item needs a name' do
+    item = Item.create name: 'Hilti'
+    assert item.persisted?
+  end
+
+  test 'item is not saved without a name' do
+    item = Item.create
+    refute item.persisted?
+  end
 end
